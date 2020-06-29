8462b18d5205dddccfff06b9403b586ca943914e
estev97 <estev97@gmail.com>
2020-06-29 13:50:26 -0600

    "add persons"

--- lib/Portfolio/ui/widgets/listview_person_inspitarion.dart
+++ lib/Portfolio/ui/widgets/listview_person_inspitarion.dart
@@ -16,8 +16,8 @@ class ListViewPersonInspiration extends StatelessWidget {
         finalColor: Color(0xFFFF5672)),
     InspirationPerson(
         name: "Albert Espinoza",
-        description: '''      ¿Un buen libro?
-        Los Secretos que jamas te contaron- Albert Espinoza''',
+        description:
+            '''¿Un buen libro? "Los Secretos que jamas te contaron" - Albert Espinoza''',
         path:
             "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/44253928_502160850263515_2213877080216954768_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=107&_nc_ohc=vU2bIKwUi6cAX9sSS0p&oh=605a69a577554a454c2e5ccd8f8787ae&oe=5F24E9A9",
         initialColor: Color(0xFF701632),
@@ -33,7 +33,7 @@ class ListViewPersonInspiration extends StatelessWidget {
     InspirationPerson(
         name: "Charles Ans",
         description:
-            '''¿Has tenido musas en tu vida? Pero esas musas las cuales te han dejado grandes enseñanzas o te inspiran a diario como tu mama, tu hermana, tu abuela, alguna profesora, tu novia o inclusiva alguna amiga, pues te recomiendo la música de Charles Ans.''',
+            '''¿Has tenido musas en tu vida? Pero esas musas las cuales te han dejado grandes enseñanzas o te inspiran a diario como tu mama, tu hermana, tu abuela, alguna profesora, tu novia o inclusive alguna amiga, pues te recomiendo que escuches el disco "Smile" de Charles Ans.''',
         path:
             "https://instagram.fgua3-1.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/55835399_214125769548150_2879143724885371293_n.jpg?_nc_ht=instagram.fgua3-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=4HT9IvQC7gIAX-wV4Oe&oh=26b15d36e55eeb139e4c4e2252c5c5e6&oe=5F2440BC",
         initialColor: Color(0xFF000428),
@@ -41,13 +41,64 @@ class ListViewPersonInspiration extends StatelessWidget {
     InspirationPerson(
         name: "Lil Supa",
         description:
-            '''¿Te consideras un melómano?, porque yo si la música ha formado parte de mi vida y si quieres algo diferente algo que te genere paz escucha: Luz-Lil supa''',
+            '''¿Te consideras un melómano?, porque yo si la música ha formado parte de mi vida y si quieres algo diferente algo que te genere paz escucha: Luz - Lil supa''',
         path:
             "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/105948596_1198679703842677_4115071016167774575_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=1&_nc_ohc=E1KmXwHZMqYAX9-SdjJ&oh=924aeed9d4e53942121caa8200695c40&oe=5F22DE0A",
         initialColor: Color(0xFF1F1C2C),
         finalColor: Color(0xFF928DAB)),
 
-    //0xFFCAC531
+    InspirationPerson(
+        name: "Elon Musk",
+        description:
+            '''"A algunas personas no les gusta el cambio, pero necesitas afrontarlo especialmente si la alternativa es un desastre" - Elon Musk''',
+        path:
+            "https://instagram.fgua3-1.fna.fbcdn.net/v/t51.2885-15/e35/72650470_177949440047738_1139886929756600594_n.jpg?_nc_ht=instagram.fgua3-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=gQ-2Hg-tTd8AX-P6Xo5&oh=0eb9e2c6d4baf708b23c3f38d78a1bd4&oe=5F24BD8E",
+        initialColor: Color(0xFF005C97),
+        finalColor: Color(0xFF363795)),
+    InspirationPerson(
+        name: "Rayden",
+        description:
+            '''¿Has olvidado a tu yo de ayer, cuando eras niño?, escucha: “A mi yo de ayer” - Rayden ''',
+        path:
+            "https://instagram.fgua3-1.fna.fbcdn.net/v/t51.2885-15/e35/87530946_1314531738934368_3493668598433508321_n.jpg?_nc_ht=instagram.fgua3-1.fna.fbcdn.net&_nc_cat=100&_nc_ohc=wlfhJ1uwbzsAX9zj_5R&oh=cea32646e6fb04008570b633c9491ae8&oe=5F230947",
+        initialColor: Color(0xFF38ef7d5),
+        finalColor: Color(0xFF11998e)),
+    InspirationPerson(
+        name: "Rapsusklei",
+        description:
+            '''¿Te llaman loco por hacer lo que amas?, escucha: “En busca de la felicidad” - Rapsusklei''',
+        path:
+            "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/72791041_1245558228964245_4980915486783898202_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=109&_nc_ohc=q-kOicAAj1IAX-TuK_G&oh=b886c5dda294d6908fd3884966dc0b71&oe=5F24F18B",
+        initialColor: Color(0xFFE5E5BE),
+        finalColor: Color(0xFF003973)),
+    InspirationPerson(
+        name: "Walter Riso",
+        description:
+            '''¿Te han dejado, la persona que te gusta te ignoro?, lee algún libro de Walter Riso el que se apegue más a tu realidad y te aseguro que te ayudara a superar ese mal momento''',
+        path:
+            "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/94331610_129322255381073_8581644126744897306_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=AGTvEIPDvBMAX-lBhfk&oh=ce2f7d8e4c853dbe7d0df42a2d108a26&oe=5F2282D3",
+        initialColor: Color(0xFF7F7FD5),
+        finalColor: Color(0xFF86A8E7)),
+
+    InspirationPerson(
+        name: "Mowlihawk",
+        description:
+            '''¿Te han dejado, la persona que te gusta te ignoro?, lee algún libro de Walter Riso el que se apegue más a tu realidad y te aseguro que te ayudara a superar ese mal momento''',
+        path:
+            "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/p1080x1080/75477009_2611335142427753_8289259036907052931_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=5gj-q5EDtkwAX8PH6CM&oh=12131aa55ec34df66c422a1ca522f595&oe=5F21D61B",
+        initialColor: Color(0xFFe1eec3),
+        finalColor: Color(0xFFf05053)),
+
+    InspirationPerson(
+        name: "Locus",
+        description:
+            '''¿Tienes un sueño? ¿Lo haces con pasión? Si no tienes dudas es que no tienes miedo y si no tienes miedos que no te importa perderlo si no te importa perderlo es que nunca fue un sueño y si nunca fue un sueño no mereces tenerlo, ¿Quieres motivación? escucha: “Respeta tus sueños” - locus ft ZPU, El chojin, Ambkor''',
+        path:
+            "https://instagram.fgua3-2.fna.fbcdn.net/v/t51.2885-15/e35/s1080x1080/71760190_3131226176925760_4498733812334718987_n.jpg?_nc_ht=instagram.fgua3-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=1mtnco0z1oUAX_ecXZ1&oh=1d97aa6e043494b45229c90d5dd32583&oe=5F22D50F",
+        initialColor: Color(0xFFfffcdc),
+        finalColor: Color(0xFFd9a7c7)),
+
+    //
   ];
 
   @override
